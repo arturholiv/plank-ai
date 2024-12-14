b21bd8c2696a7c2698f441290fb06b7a710eeb94
arturholiv <artur.henrique45@hotmail.com>
2024-12-14 14:03:28 -0300

    feat (backend): adds cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { enhanceCode } from './openAI';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 dotenv.config();
 
 app.get('/', (req: Request, res: Response) => {
